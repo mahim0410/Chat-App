19bd2efb03ea51767968c3248c21e6659c68dc06
mahim0410 <mahidulislam3397@gmail.com>
2026-03-13 03:16:47 +0600

    Deployment setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import 'dotenv/config';
+import path from "path"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 
 const app = express()
+const __dirname = path.resolve();
+
 const port = process.env.PORT
 
-app.listen(port, () => {
-    console.log(`server is running smoothly`)
-})
+
 
 app.use('/api/auth', authRoutes)
 app.use("/api/message", messageRoutes)
 
 
+// for deployement
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
+
+
+
+
+app.listen(port, () => {
+    console.log(`server is running smoothly`)
+})
